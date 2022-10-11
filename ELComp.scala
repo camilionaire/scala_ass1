31a1e74e39ca47c58ae33061d3d461ec2f9ad601
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-10-11 08:02:02 -0700

    init commit

---
+++ ELComp.scala
@@ -0,0 +1,64 @@
+// ExpLang to StackM1 Compiler
+//
+// Usage: linux> scala ELComp <source file>
+//
+//
+import ExpLang._
+import StackM1._
+
+object ELComp {
+  var nextLabel: Int = 100
+
+  case class CompileException(string: String) extends RuntimeException
+
+  def compile(e: Expr): Program = e match {
+    case True => Const(1)::Nil
+    case False => Const(0)::Nil
+    case Num(n) => Const(n)::Nil
+    case Not(e) => // ... need code ...
+    case And(l,r) => // ... need code ...
+    case Or(l,r)  => // ... need code ...
+    case Xor(l,r) => // ... need code ...
+    case Add(e1,e2) => // ... need code ...
+    case Sub(e1,e2) => // ... need code ...
+    case Mul(e1,e2) => // ... need code ...
+    case Div(e1,e2) => // ... need code ...
+    case Rem(e1,e2) => // ... need code ...
+    case Gt(e1,e2)  => // ... need code ...
+    case Lt(e1,e2)  => // ... need code ...
+    case Eq(e1,e2)  => // ... need code ...
+    case If(c,t,f) => // ... need code ...
+    case _ => throw CompileException("Illegal expr:" + e)
+  }
+
+  def newLabel() = {
+    val next = nextLabel
+    nextLabel = nextLabel + 1
+    next
+  }
+
+  def apply(s:String, debug:Int = 0) = {
+    if (debug > 0) println("Input:  " + s)
+    val e = ELParse(s)
+    if (debug > 0) println("AST:    " + e)
+    val p: Program = compile(e)
+    if (debug > 0) println("Target: " + p)
+    exec(p,debug)
+  }
+
+  // Test driver
+  import scala.io.Source
+  def main(argv: Array[String]) = {
+    try {
+      val s = Source.fromFile(argv(0)).getLines.mkString("\n")
+      val d = if (argv.length > 1) argv(1).toInt else 0
+      val v = apply(s,d)
+      println(v)
+    } catch {
+      case ex: ParseException => println("Parser Error: " + ex.string)
+      case ex: CompileException => println("Compile Error: " + ex.string)
+    }
+    ()
+  }
+}
+//
---
+++ ELInterp.scala
@@ -0,0 +1,55 @@
+// ExpLang Interpreter
+//
+// Usage: linux> scala ELInterp <source file>
+//
+//
+import ExpLang._
+
+object ELInterp {
+  case class InterpException(string: String) extends RuntimeException
+
+  def interp(e:Expr): Either[Boolean,Int] = e match {
+    case True => Left(true)
+    case False => Left(false)
+    case Num(n) => Right(n)
+    case Not(e)  => // ... need code ...
+    case And(l,r) => // ... need code ...
+    case Or(l,r)  => // ... need code ...
+    case Xor(l,r) => // ... need code ...
+    case Add(l,r) => // ... need code ...
+    case Sub(l,r) => // ... need code ...
+    case Mul(l,r) => // ... need code ...
+    case Div(l,r) => // ... need code ...
+    case Rem(l,r) => // ... need code ...
+    case Lt(l,r) => // ... need code ...
+    case Gt(l,r) => // ... need code ...
+    case Eq(l,r) => // ... need code ...
+    case If(c,l,r) => // ... need code ... 
+    case _ => throw InterpException("Illegal expr:" + e)
+  }
+
+  def apply(s:String, debug:Int = 0) = {
+    if (debug > 0) println("Input:  " + s)
+    val e = ELParse(s)
+    if (debug > 0) println("AST:    " + e)
+    interp(e)
+  }
+
+  // Test driver
+  import scala.io.Source
+  def main(argv: Array[String]) = {
+    try {
+      val s = Source.fromFile(argv(0)).getLines.mkString("\n")
+      val d = if (argv.length > 1) argv(1).toInt else 0
+      val v = apply(s,d)
+      v match {
+        case Left(b)  => printf(b+"\n")
+        case Right(i) => printf(i+"\n")
+      }
+    } catch {
+      case ex: ParseException =>  println("Parser Error: " + ex.string)
+      case ex: InterpException => println("Interp Error: " + ex.string)
+    }
+  }
+}
+//
